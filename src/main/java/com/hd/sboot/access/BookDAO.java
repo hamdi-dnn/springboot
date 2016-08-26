7f2cd2b0badd51c23548214c8917ba9e6afff266
hdannouni <hamdi.dannouni@amadeus.com>
2016-08-26 14:50:01 +0200

    refactor access and action/controller packages

--- src/main/java/com/hd/sboot/access/BookDAO.java
+++ src/main/java/com/hd/sboot/access/BookDAO.java
@@ -1,4 +1,4 @@
-package com.hd.sboot;
+package com.hd.sboot.access;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
--- src/main/java/com/hd/sboot/controllers/Example.java
+++ src/main/java/com/hd/sboot/controllers/Example.java
@@ -1,4 +1,4 @@
-package com.hd.sboot;
+package com.hd.sboot.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
